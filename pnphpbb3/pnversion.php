9bfb5fa1f25100777a533c53194593ae0a16e733
tettamanti <tettamanti@9535a083-e512-0410-9162-9adae2548661>
2008-09-28 16:29:32 +0000

    Add pnversion.php (zk module definition)

---
+++ pnphpbb3/pnversion.php
@@ -0,0 +1,17 @@
+<?php
+
+$modversion['name']				= 'Zafenio';
+$modversion['displayname']		= 'Zafenio';
+$modversion['description']		= 'Zafenio';
+$modversion['version']			= '2.9.0';
+$modversion['credits']			= 'pndocs/credits.txt';
+$modversion['help']				= 'pndocs/install.txt';
+$modversion['changelog']		= 'pndocs/changelog.txt';
+$modversion['license']			= 'pndocs/license.txt';
+$modversion['official']			= 0;
+$modversion['author']			= 'Zafenio Team, original code from phpBB3';
+$modversion['contact']			= 'http://www.zafenio.com';
+$modversion['admin']			= 0;
+$modversion['securityschema']	= array();
+
+?>
