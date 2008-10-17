9ffcf9bd4396e4382d17ac3dbde725b1763d3ada
slam <slam@9535a083-e512-0410-9162-9adae2548661>
2008-10-17 09:58:43 +0000

    fixed password field lenght in PNphpBB2, prepare correct inital version for zafenio

--- pnphpbb3/pnversion.php
+++ pnphpbb3/pnversion.php
@@ -1,17 +1,18 @@
 <?php
 
-$modversion['name']				= 'Zafenio';
+$modversion['name']			= 'Zafenio';
+$modversion['oldnames']			= array'PNphpBB2';
 $modversion['displayname']		= 'Zafenio';
 $modversion['description']		= 'Zafenio';
 $modversion['version']			= '2.9.0';
 $modversion['credits']			= 'pndocs/credits.txt';
-$modversion['help']				= 'pndocs/install.txt';
+$modversion['help']			= 'pndocs/install.txt';
 $modversion['changelog']		= 'pndocs/changelog.txt';
 $modversion['license']			= 'pndocs/license.txt';
 $modversion['official']			= 0;
 $modversion['author']			= 'Zafenio Team, original code from phpBB3';
 $modversion['contact']			= 'http://www.zafenio.com';
 $modversion['admin']			= 0;
-$modversion['securityschema']	= array();
+$modversion['securityschema']		= array();
 
 ?>
