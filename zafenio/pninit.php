919f3a4a74b9d7c25ec842ca8e867f60bee1d080
slam <slam@9535a083-e512-0410-9162-9adae2548661>
2010-05-25 08:17:37 +0000

    some more meat to installer

--- zafenio/pninit.php
+++ zafenio/pninit.php
@@ -1,8 +1,42 @@
 <?php
 
-function pnphpbb3_init() {
+function zafenio_init() {
 	/* TODO */
+	$dom = ZLanguage::getModuleDomain('zafenio');
 	return LogUtil::registerStatus('Stub only');
+}
+
+function zafenio_upgrade()
+{
+	/* TODO */
+	$dom = ZLanguage::getModuleDomain('zafenio');
+	return LogUtil::registerStatus('Stub only');
+}
+
+function zafenio_delete()
+{
+	$dom = ZLanguage::getModuleDomain('zafenio');
+	/* Delete the module tables - example, still TODO */
+	if (!DBUtil::dropTable('zafenio_pages')) {
+	return LogUtil::registerError(__('Error! Table deletion failed.', $dom).' (zafenio_pages)');
+	}
+
+	if (!DBUtil::dropTable('zafenio_links')) {
+	return LogUtil::registerError(__('Error! Table deletion failed.', $dom).' (zafenio_links)');
+	}
+
+	if (!DBUtil::dropTable('zafenio_referrers')) {
+	return LogUtil::registerError(__('Error! Table deletion failed.', $dom).' (zafenio_referrers)');
+	}
+
+	/* Delete the module vars */
+	pnModDelVar('zafenio');
+
+	/* Delete the Admin module register */
+	pnModDBInfoLoad('Admin');
+	DBUtil::deleteObjectByID('admin_module', (int)pnModGetIdFromName('zafenio'), 'mid');
+
+	return true;
 }
 
 ?>
